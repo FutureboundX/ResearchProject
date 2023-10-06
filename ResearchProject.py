71cfb13ebc1b35fdce407957aded86e31fbeff4d
FutureboundX <48761556+FutureboundX@users.noreply.github.com>
2023-10-06 02:35:49 -0400

    Update ResearchProject.py

--- ResearchProject.py
+++ ResearchProject.py
@@ -5,9 +5,9 @@ from nltk.corpus import stopwords
 
 #Test Corpus
 corpus = [
-    "I love natural language processing.",
-    "Word embeddings are fascinating.",
-    "Machine learning is important for AI."
+    "The Content Marketing Coordinator is responsible for designing various types of media content (print, digital, social etc.) to generate content and drive marketing campaigns",
+    "This role will execute and organize daily administrative tasks and provide project support for the marketing team, including assisting with trade shows, managing creative requests, delivering exceptional customer support and collaborating with internal teams to achieve desired marketing deliverables.",
+    "This position reports to the Marketing Communications Manager."
 ]
 
 #Tokenize the sentences and remove stopwords
@@ -22,9 +22,9 @@ model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, sg=0)
 model.save("word2vec_model")
 
 #Example: Getting word embeddings
-vector = model.wv["love"]
-print("Embedding for 'love':", vector)
+vector = model.wv["marketing"]
+print("Embedding for 'marketing':", vector)
 
 #Example: Finding similar words
-similar_words = model.wv.most_similar("love", topn=3)
-print("Words similar to 'love':", similar_words)
+similar_words = model.wv.most_similar("marketing", topn=3)
+print("Words similar to 'marketing':", similar_words)
